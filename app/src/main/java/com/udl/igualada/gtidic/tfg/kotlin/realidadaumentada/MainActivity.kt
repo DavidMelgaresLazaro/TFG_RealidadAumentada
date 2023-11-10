9d67e2bec05e36723a6080af277995304199d051
davidmelgares <davidmelgareslazaro@gmail.com>
2023-11-10 10:44:41 +0100

    init

---
+++ app/src/main/java/com/udl/igualada/gtidic/tfg/kotlin/realidadaumentada/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.udl.igualada.gtidic.tfg.kotlin.realidadaumentada
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ app/src/test/java/com/udl/igualada/gtidic/tfg/kotlin/realidadaumentada/ExampleUnitTest.kt
@@ -0,0 +1,17 @@
+package com.udl.igualada.gtidic.tfg.kotlin.realidadaumentada
+
+import org.junit.Test
+
+import org.junit.Assert.*
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+class ExampleUnitTest {
+    @Test
+    fun addition_isCorrect() {
+        assertEquals(4, 2 + 2)
+    }
+}
