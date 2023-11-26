820a763fdfb644259c2696dd8170790fb0b1875b
davidmelgares <davidmelgareslazaro@gmail.com>
2023-11-26 13:42:46 +0100

    funciona la camara

--- app/src/main/java/com/udl/igualada/gtidic/tfg/kotlin/realidadaumentada/MainActivity.kt
+++ app/src/main/java/com/udl/igualada/gtidic/tfg/kotlin/realidadaumentada/MainActivity.kt
@@ -1,11 +1,24 @@
 package com.udl.igualada.gtidic.tfg.kotlin.realidadaumentada
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.FragmentContainerView
+import com.google.ar.sceneform.ux.ArFragment
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        val arFragmentContainer = findViewById<FragmentContainerView>(R.id.activity_main__container__camera_area)
+
+        arFragmentContainer?.let {
+            val arFragment = ArFragment()
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.activity_main__container__camera_area, arFragment)
+                .commit()
+        }
     }
 }
