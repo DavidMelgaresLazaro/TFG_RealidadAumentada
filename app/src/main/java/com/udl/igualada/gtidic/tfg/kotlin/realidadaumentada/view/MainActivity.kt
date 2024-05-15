cc3830aa3c20a0c6887f82c1b2bf522c79f97ba4
JordiMateoUdL <jordi.mateo@udl.cat>
2024-05-15 22:43:26 +0200

    @fix: The user can load and change the model using the button, and loading from local storage

--- app/src/main/java/com/udl/igualada/gtidic/tfg/kotlin/realidadaumentada/view/MainActivity.kt
+++ app/src/main/java/com/udl/igualada/gtidic/tfg/kotlin/realidadaumentada/view/MainActivity.kt
@@ -24,13 +24,24 @@ import android.util.Log
 import android.view.GestureDetector
 import android.view.MotionEvent
 import android.view.View
+import com.google.ar.core.Anchor
+import com.google.ar.core.Plane
+import com.google.ar.sceneform.AnchorNode
 import com.google.ar.sceneform.math.Vector3
+import com.google.ar.sceneform.rendering.ModelRenderable
+import com.google.ar.sceneform.rendering.Renderable
 import com.google.ar.sceneform.ux.TransformableNode
 import com.udl.igualada.gtidic.tfg.kotlin.realidadaumentada.R
 import com.udl.igualada.gtidic.tfg.kotlin.realidadaumentada.viewmodel.ArViewModel
 import java.io.*
+import java.util.concurrent.CompletableFuture
 
 class MainActivity : AppCompatActivity() {
+
+    sealed class ModelSource {
+        data class ResourceId(val id: Int) : ModelSource()
+        data class UriSource(val uri: Uri) : ModelSource()
+    }
 
     companion object {
         private const val PERMISSION_REQUEST_CODE = 1
@@ -41,6 +52,12 @@ class MainActivity : AppCompatActivity() {
     private lateinit var arFragment: ArFragment
 
     private var lastHitResult: HitResult? = null
+
+    private var currentAnchorNode: AnchorNode? = null
+    private var currentTransformableNode: TransformableNode? = null
+
+    private lateinit var selectedObject: ModelSource
+    private lateinit var selectedObject2: Uri
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -55,27 +72,109 @@ class MainActivity : AppCompatActivity() {
             ViewModelProvider.AndroidViewModelFactory.getInstance(application)
         ).get(ArViewModel::class.java)
 
-        if (checkSystemSupport()) {
+        selectedObject = ModelSource.ResourceId(R.raw.sas__cs2_agent_model_green)
+
+
+
+        /*if (checkSystemSupport()) {
             setOnTapInPlane()
-            setupTakePhotoButton()
             setupSelectModelButton()
             setupIncreaseSizeButton()
             setupDecreaseSizeButton()
-            setupGestureDetector()
+        }*/
+        arFragment.setOnTapArPlaneListener { hitResult, plane, _ ->
+            //If surface is not horizontal and upward facing
+            if (plane.type != Plane.Type.HORIZONTAL_UPWARD_FACING) {
+                //return for the callback
+                return@setOnTapArPlaneListener
+            }
+            //create a new anchor
+            val anchor = hitResult.createAnchor()
+            placeObject(arFragment, anchor, selectedObject as ModelSource)
+        }
+
+        setupSelectModelButton()
+        setupIncreaseSizeButton()
+        setupDecreaseSizeButton()
+
+    }
+
+    private fun placeObject(fragment: ArFragment, anchor: Anchor, model: ModelSource) {
+        Log.d("ArViewModel", "Selected 3D model: $model")
+        val context = fragment.requireContext()
+
+        val renderableFuture: CompletableFuture<ModelRenderable>? = when (model) {
+            is ModelSource.ResourceId ->
+                ModelRenderable.builder().setSource(context, model.id).setIsFilamentGltf(true).build()
+            is ModelSource.UriSource -> ModelRenderable.builder().setSource(context, model.uri).setIsFilamentGltf(true).build()
+        }
+
+        renderableFuture?.thenAccept { renderable ->
+            Log.d("ArViewModel", "ModelRenderable instance: $renderable")
+            addNodeToScene(fragment, anchor, renderable)
+        }?.exceptionally { throwable ->
+            Log.d("ArViewModel", "Error creating model renderable", throwable)
+            Toast.makeText(context, context.getString(R.string.something_is_not_right)
+                    + throwable.message, Toast.LENGTH_LONG).show()
+            null
         }
     }
+
+    private fun addNodeToScene(fragment: ArFragment, anchor: Anchor, model: ModelRenderable) {
+        Log.d("ArViewModel", "Anchor: $anchor")
+        Log.d("ArViewModel", "ModelRenderable: $model")
+        try {
+            val anchorNode = AnchorNode(anchor)
+            val transformableNode = TransformableNode(fragment.transformationSystem)
+            transformableNode.renderable = model
+            Log.d("ArViewModel", "TransformableNode instance: $transformableNode")
+            transformableNode.setParent(anchorNode)
+            fragment.arSceneView.scene.addChild(anchorNode)
+            transformableNode.select()
+            Log.d("ArViewModel", "Node added to scene successfully")
+            Log.d("ArViewModel", "Node scale: ${transformableNode.localScale}")
+            Log.d("ArViewModel", "Node position: ${transformableNode.localPosition}")
+
+            currentAnchorNode = anchorNode
+            currentTransformableNode = transformableNode
+
+        } catch (e: Exception) {
+            Log.e("ArViewModel", "Error adding node to scene", e)
+        }
+    }
+
+
+
 
     private fun setupIncreaseSizeButton() {
         val increaseSizeButton = findViewById<Button>(R.id.btnIncreaseSize)
         increaseSizeButton.setOnClickListener {
-            arViewModel.increaseModelSize(arFragment)
+            //arViewModel.increaseModelSize(arFragment)
+            increaseModelSize()
         }
     }
 
     private fun setupDecreaseSizeButton() {
         val decreaseSizeButton = findViewById<Button>(R.id.btnDecreaseSize)
         decreaseSizeButton.setOnClickListener {
-            arViewModel.decreaseModelSize(arFragment)
+            //arViewModel.decreaseModelSize(arFragment)
+            decreaseModelSize()
+        }
+    }
+
+    private fun increaseModelSize() {
+        currentTransformableNode?.let { transformableNode ->
+            val currentScale = transformableNode.localScale
+            val newScale = Vector3(currentScale.x * 1.1f, currentScale.y * 1.1f, currentScale.z * 1.1f)
+            transformableNode.localScale = newScale
+        }
+    }
+
+    private fun decreaseModelSize() {
+        currentTransformableNode?.let { transformableNode ->
+            val currentScale = transformableNode.localScale
+            val newScale = Vector3(currentScale.x * 0.9f, currentScale.y * 0.9f, currentScale.z * 0.9f)
+            transformableNode.localScale = newScale
         }
     }
 
@@ -134,10 +233,35 @@ class MainActivity : AppCompatActivity() {
             takePhoto()
         }
     }
+
+    private fun setModelPath(modelFileName: String) {
+        selectedObject2 = Uri.parse(modelFileName)
+        val toast = Toast.makeText(applicationContext, modelFileName, Toast.LENGTH_SHORT)
+        toast.show()
+    }
+
+    private fun updateCurrentSceneWithNewModel(model: ModelSource){
+
+        // Remove the existing TransformableNode from its parent AnchorNode
+        currentTransformableNode?.let {
+            it.parent?.let { parent ->
+                parent.removeChild(it)
+            }
+        }
+
+        // Create a new TransformableNode with the new model and add it to the same AnchorNode
+        currentAnchorNode?.let { anchorNode ->
+            anchorNode.anchor?.let { placeObject(arFragment, it, model) }
+        }
+
+    }
+
+
     private fun setupSelectModelButton() {
         val selectModelButton = findViewById<Button>(R.id.btnSelectModel)
         selectModelButton.setOnClickListener {
             openFileSelector()
+            //selectedObject = ModelSource.ResourceId(R.raw.lamp)
         }
     }
     private fun openFileSelector() {
@@ -153,7 +277,9 @@ class MainActivity : AppCompatActivity() {
         if (requestCode == PICK_MODEL_REQUEST_CODE && resultCode == RESULT_OK) {
             data?.data?.let { uri ->
                 val fp = getFilePathFromUri(uri)
-               arViewModel.changeModel(fp, arFragment)
+                selectedObject = ModelSource.UriSource(Uri.parse(fp))
+                updateCurrentSceneWithNewModel(selectedObject)
+
             }
         }
     }
